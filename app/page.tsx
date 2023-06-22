2bd42bc524eb4a64cc536716ac7ffcdbf41835e7
Mukul <mukulphoughat@gmail.com>
2023-06-22 14:33:07 +0530

    Add All

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import {SignIn} from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <main className="flex flex-row min-h-screen w-full items-center justify-between p-24">
-      <div className={"w-1/3 min-h-screen bg-gray-900"}>
+    <main className="flex flex-row h-full w-full items-center justify-between p-24">
+      <div className={"w-2/6 min-h-screen bg-gray-900"}>
 
       </div>
-        <div className={"w-2/3"}>
+        <div className={"w-4/6"}>
             {/*<SignIn/>*/}
         </div>
     </main>
