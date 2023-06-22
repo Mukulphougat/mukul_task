f6e32e5fa7cedeecc53c737d15cae040c1854e6e
Mukul <mukulphoughat@gmail.com>
2023-06-22 14:36:22 +0530

    Add All

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import {SignIn} from "@clerk/nextjs";
 export default function Home() {
   return (
     <main className="flex flex-row h-full w-full items-center justify-between p-24">
-      <div className={"w-2/6 h-full bg-gray-900"}>
-
+      <div className={"w-2/6 grid place-items-center h-full bg-gray-900"}>
+        <h1 className={"text-white text-3xl"}>Board</h1>
       </div>
         <div className={"w-4/6"}>
             {/*<SignIn/>*/}
