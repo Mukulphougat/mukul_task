3f123748b647fbcef91c311123868956070db83c
Mukul <mukulphoughat@gmail.com>
2023-06-26 22:43:06 +0530

    Final Commit

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,4 +1,3 @@
-// "use client"
 import {Lato, Montserrat} from "next/font/google";
 import {currentUser, SignedIn, SignedOut, SignIn} from "@clerk/nextjs";
 import type {User} from "@clerk/nextjs/api"
@@ -8,7 +7,7 @@ import SignOutButtonForProfile from "@/components/SignOutButtonForProfile";
 
 const montserrat=Montserrat({subsets: ['latin']})
 const latoFont=Lato({weight:"300",subsets: ['latin']})
-export default async function Profile(props: { children: React.ReactNode }) {
+export default async (props: { children: React.ReactNode }) => {
     let {children} = props;
     const user: User | null = await currentUser();
 
@@ -47,18 +46,19 @@ export default async function Profile(props: { children: React.ReactNode }) {
                         <div className={"w-full bg-transparent rounded-lg h-4/5 my-6 grid place-items-center"}>
                             <div className={"w-full flex flex-col rounded-lg h-full bg-white"}>
                                 <div className={"w-full flex flex-row justify-center my-10"}>
-                                    <img width={150} height={150} className={"rounded-full"} src={user !== null ? user.imageUrl : ""} alt={"User Profile"}/>
+                                    <img width={150} height={150} className={"rounded-full"}
+                                         src={user !== null ? user.imageUrl : ""} alt={"User Profile"}/>
                                 </div>
                                 <div className={"w-full flex flex-row justify-center my-5"}>
-                                    <h1 className={"text-2xl font-medium "+montserrat.className}>{user?.firstName+" "+user?.lastName}</h1>
+                                    <h1 className={"text-2xl font-medium " + montserrat.className}>{user?.firstName + " " + user?.lastName}</h1>
                                 </div>
                                 <div className={"w-full flex flex-row justify-center my-5"}>
-                                    <h1 className={"text-2xl text-blue-400 underline hover:no-underline font-medium "+montserrat.className}>
+                                    <h1 className={"text-2xl text-blue-400 underline hover:no-underline font-medium " + montserrat.className}>
                                         <a href={`mailto:${user?.emailAddresses[0].emailAddress}`}>{user?.emailAddresses[0].emailAddress}</a>
                                     </h1>
                                 </div>
                                 <div className={"w-full flex flex-row justify-center my-5"}>
-                                    <SignOutButtonForProfile />
+                                    <SignOutButtonForProfile/>
                                 </div>
                             </div>
                         </div>
